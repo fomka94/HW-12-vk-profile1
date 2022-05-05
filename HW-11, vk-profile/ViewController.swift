da7c3f3472903d6708300b977b3b29772c48f2fa
fomka94 <fom-aina@mail.ru>
2022-05-05 22:05:47 +0900

    Initial Commit

---
+++ HW-11, vk-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-11, vk-profile
+//
+//  Created by Фомина Айина Валерьевна on 05.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
